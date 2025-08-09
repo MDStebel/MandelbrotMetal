a9855f16808c5a0281b4ea7ff932b28e5c609ed1
Michael Stebel <mdstebel@gmail.com>
2025-08-08 23:24:50 -0400

    Initial Commit

---
+++ Mandelbrot/MandelbrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandelbrotApp.swift
+//  Mandelbrot
+//
+//  Created by Michael Stebel on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MandelbrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
